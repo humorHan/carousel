a5560a91bc40d1a3af750b9deed38d2f172d87f2
humorHan <humorHan@users.noreply.github.com>
2016-12-01 15:57:47 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -4,10 +4,8 @@
 require("common/base.less");
 var testCarousel = require("component/carousel/carousel.js");
 
-
-
 testCarousel({
     dom: 'dom1',
-    data: data.banners,
+    data: data.banners,  //此处为ajax数据...
     hasPreNext: true
 });
