4740bed66850558eed241f378fe72a391cb8c303
humorHan <humorHan@users.noreply.github.com>
2016-12-01 16:06:55 +0800

    Add files via upload

---
+++ dep/config/config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by humorHan on 16/11/18.
+ */
+module.exports = {
+    //七牛
+    //SOURCE: 'http://source.soyyin.com/',
+    //测试服 发布正式版要更改
+    SOURCE: 'http://7xsolp.com1.z0.glb.clouddn.com/',
+    //请求首页banner的数量
+    BANNER_NUM: 6,
+    //楼层数
+    FLOOR_NUM: 4
+};
